c9adaecd9c1bccaacde3e97309d778ce35e0331e
Prajwal Belagatti <prajwal.be.21@gmail.com>
2021-07-26 16:07:06 +0530

    Add books model

---
+++ src/models/books.ts
@@ -0,0 +1,61 @@
+import client from "../db";
+
+export type Book = {
+    id: number;
+    title: string;
+    total_pages: number;
+    author: string;
+    type: string;
+    summary: string;
+};
+
+export class BookStore {
+    async index(): Promise<Book[]> {
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM books';
+            const result = await conn.query(sql);
+            conn.release;
+            return result.rows;
+        } catch(err) {
+            throw new Error(`Could not get books. Error: ${err}`)
+        }
+    }
+
+    async show(id: string): Promise<Book> {
+        try {
+            const conn = await client.connect();
+            const sql = 'SELECT * FROM books WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release;
+            return result.rows[0];
+        } catch(err) {
+            throw new Error(`Could not get books. Error: ${err}`)
+        }
+    }
+
+    async create(b: Book): Promise<Book> {
+        try {
+            const conn = await client.connect();
+            const sql = 'INSERT INTO books (title, author, total_pages, summary) VALUES($1, $2, $3, $4) RETURNING *';
+            const result = await conn.query(sql, [b.title, b.author, b.total_pages, b.summary]);
+            const book = result.rows[0];
+            conn.release()
+            return book;
+        } catch(err) {
+            throw new Error(`Could not get books. Error: ${err}`)
+        }
+    }
+
+    async delete(id: string): Promise<Book> {
+        try {
+            const conn = await client.connect();
+            const sql = 'DELETE FROM books WHERE id=($1)';
+            const result = await conn.query(sql, [id]);
+            conn.release;
+            return result.rows[0];
+        } catch(err) {
+            throw new Error(`Could not get books. Error: ${err}`)
+        }
+    }
+};
