7d3bc400562bdf3a0615abefe88b44dd7556ebb0
Siddube <prajwal.be.21@gmail.com>
2021-08-02 15:07:57 +0530

    Add JWT

--- src/handlers/users-route.ts
+++ src/handlers/users-route.ts
@@ -1,5 +1,16 @@
 import express from 'express';
+import  jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
 import { User, UserStore } from '../models/users';
+
+dotenv.config();
+
+const { 
+    TOKEN_SECRET, 
+} = process.env;
+
+const jwtSecret: unknown = TOKEN_SECRET;
 
 const store = new UserStore();
 
@@ -9,22 +20,41 @@ const index = async (req: express.Request,res: express.Response)=>{
 }
 
 const create= async (req:express.Request,res:express.Response)=>{
-    try{
+    try {
         const user:User={ id:req.body.id,
             username:req.body.username,
             password:req.body.password
         }
         const newUser= await store.create(user);
-        res.json(newUser);
-    }catch(err){
+        var token = jwt.sign({ user: newUser }, jwtSecret as string);
+        res.json(token)
+    } catch(err) {
         res.status(400)
         res.json(err)
-    }  
+    }
 }
 
-const bookRoutes = (app:express.Application) => {
+const authenticate = async (req: express.Request, res: express.Response) => {
+    const user: User = { id:req.body.id,
+      username: req.body.username,
+      password: req.body.password,
+    }
+    try {
+        const u = await store.authenticate(user.username, user.password)
+        var token = jwt.sign({ user: u }, jwtSecret as string);
+        res.json(token)
+    } catch(error) {
+        res.status(401)
+        res.json({ error })
+    }
+  }
+
+
+
+const userRoutes = (app:express.Application) => {
     app.get('/users', index);
-    app.post('/users',create)
+    app.post('/users/register',create);
+    app.post('/users/login',authenticate)
 };
 
-export default bookRoutes;
+export default userRoutes;
